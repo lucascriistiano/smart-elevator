210fb0dd1d4ebe35bde95177e824bf758a9c5f14
Lucas Cristiano Calixto Dantas <lucascristiano27@gmail.com>
2017-06-27 02:41:32 -0300

    Change and remove functions

--- lang/c/ElevatorController.h
+++ lang/c/ElevatorController.h
@@ -27,6 +27,7 @@ extern void ElevatorController__INITIALISATION(void);
 extern void ElevatorController__elev_current_floor(int32_t ee, int32_t *ff);
 extern void ElevatorController__elev_current_weight(int32_t ee, int32_t *ww);
 extern void ElevatorController__elev_door_state(int32_t ee, Elevator_ctx__DOOR_STATE *ss);
+extern void ElevatorController__elev_direction(int32_t ee, Elevator_ctx__DIRECTION *dir);
 extern void ElevatorController__elev_call_elevator(int32_t call_floor, int32_t dest_floor, int32_t user, int32_t *elnum);
 extern void ElevatorController__elev_open_door(int32_t ee, int32_t ff);
 extern void ElevatorController__elev_close_door(int32_t ee, int32_t ff);
@@ -34,8 +35,6 @@ extern void ElevatorController__elev_enter(int32_t ee, int32_t uu);
 extern void ElevatorController__elev_exit(int32_t ee, int32_t uu);
 extern void ElevatorController__elev_go_up(int32_t ee);
 extern void ElevatorController__elev_go_down(int32_t ee);
-extern void ElevatorController__elev_query_direction(int32_t ee, Elevator_ctx__DIRECTION *dir);
-extern void ElevatorController__elev_calc_direction(int32_t orig_floor, int32_t dest_floor, Elevator_ctx__DIRECTION *dir);
 
 #ifdef __cplusplus
 }
